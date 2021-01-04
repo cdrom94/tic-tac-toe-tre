0e96e98dc8e5428ea51964d9f61adbf2f56a1afa
cdrom94 <sadiaromain@gmail.com>
2021-01-04 16:40:14 -0500

    add Flare and rearrange components

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, Suspense, useRef, useCallback } from 'react'
 import { Canvas } from 'react-three-fiber'
-import { OrbitControls, Loader } from "drei"
-import calculateWinner from "./calculateWinner"
+import {OrbitControls} from '@react-three/drei/OrbitControls'
+import {Loader} from '@react-three/drei/Loader'
+import calculateWinner from './calculateWinner'
+// import {Environment} from '@react-three/drei/Environment'
+// import HDRI from './parkimage.hdr'
 
-import Lighting from './Lighting'
-import Game from './Game'
-import PostProcessing from './PostProcessing'
-import Info from './Info'
-import Grid from './Grid'
-import Environment from './Environment'
+import Environment from './components/Environment'
+import Flare from './components/Flare'
+import Game from './components/Game'
+import Grid from './components/Grid'
+import Info from './components/Info'
+import Lighting from './components/Lighting'
+import Particles from './components/Particles'
+import PostProcessing from './components/PostProcessing'
 
 const App = () => {
     const [board, setBoard] = useState(Array(9).fill(null))
@@ -18,7 +23,7 @@ const App = () => {
     const handleClick = i => {
         const boardCopy = [...board]
         if ( winner || boardCopy[i] ) return
-        boardCopy[i] = xIsNext ? 'Gem' : "Pearl"
+        boardCopy[i] = xIsNext ? 'Gem' : 'Pearl'
         setBoard(boardCopy)
         setXIsNext(!xIsNext)
     }
@@ -29,22 +34,27 @@ const App = () => {
 
     return(
         <React.Fragment>
-            <Canvas camera={{ fov: 50, position: [4.19303804240389, -1.4338184309228788, -12.861740834904932]}}>
-                <OrbitControls />
-                <Lighting />
-                <Environment />
-                <Grid />
-                <Game board={board} xIsNext={xIsNext} winner={winner} handleClick={handleClick}/>
-                <PostProcessing />
-            </Canvas>
-            <Loader 
-                containerStyles={{background: "white"}}
-                innerStyles={{background: "rgba(244, 199, 238, 0.76)", height: 45, width: 300}}
-                barStyles={{background: "white", height: 45}}
-                dataStyles={{marginTop: "0.5em", color: "white", fontSize: "1em", fontFamily: "sans-serif", textTransform: "uppercase", fontWeight: 900}}
-                dataInterpolation={p => `Loading...`}
-            />
-            <Info board={board} xIsNext={xIsNext} winner={winner} renderMoves={renderMoves}/>
+                <Suspense fallback={null}>
+                    <Loader 
+                        containerStyles={{background: "white"}}
+                        innerStyles={{background: "rgba(244, 199, 238, 0.76)", height: 45, width: 300}}
+                        barStyles={{background: "white", height: 45}}
+                        dataStyles={{color: "rgba(244, 199, 238, 0.76)", fontSize: "1em", fontFamily: "sans-serif", textTransform: "uppercase", fontWeight: 900, float: "right"}}
+                        dataInterpolation={() => `Loading`} />
+                    <Canvas camera={{ fov: 50, position: [-8.19303804240389, -1.4338184309228788, -12.861740834904932]}}>
+                            <OrbitControls maxDistance={60} enableDamping/>
+                            <Lighting />
+                            <Grid />
+                            <Game board={board} xIsNext={xIsNext} winner={winner} handleClick={handleClick}/>
+                            <Particles count={300} color="pink"/>
+                            <Suspense fallback={null}>
+                                <Flare />
+                            </Suspense> 
+                            <PostProcessing />
+                            <Environment />
+                    </Canvas>
+                    <Info board={board} xIsNext={xIsNext} winner={winner} renderMoves={renderMoves}/>
+                </Suspense>
         </React.Fragment>
     )
 }
